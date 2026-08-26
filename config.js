5c01f92376be9da4684de02831dc9039dfcc7c9d
Aomsin-Market <aomsin-n.s.market@outlook.co.th>
2026-08-27 01:17:59 +0700

    เชื่อมเว็บไซต์กับ Google Sheets

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // หลังจากเผยแพร่ Google Apps Script แล้ว ให้นำ URL ที่ลงท้ายด้วย /exec มาใส่ตรงนี้
 window.SMILE_SHIRT_CONFIG = {
-  appsScriptUrl: "",
-  demoMode: true
+  appsScriptUrl: "https://script.google.com/macros/s/AKfycbzP1a5dZw76ghFQjifYvAVKc6OqStDWcaKoICtWi6pPJuXqacnw4AXsVywnkM02NzRG9w/exec",
+  demoMode: false
 };
